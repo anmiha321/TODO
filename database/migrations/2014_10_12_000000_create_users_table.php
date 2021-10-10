bff2ae1b541661fe653d041ca56fdef644577dac
anmihys <anmiha46@rambler.ru>
2021-10-10 23:33:27 +0500

    yes

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('surname');
+            $table->string('name');
+            $table->string('patronymic');
+            $table->string('Email');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->boolean('type')->default(0)->nullable();
+            $table->timestamps();
+            $table->rememberToken();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
